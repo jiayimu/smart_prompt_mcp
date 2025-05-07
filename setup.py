5dd4ffae771534e650ac9c57cd830f901c25d00d
jiayimu <jiayimu@users.noreply.github.com>
2025-05-07 17:56:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="smart_prompt_mcp",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     install_requires=[
         "fastmcp",
